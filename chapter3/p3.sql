6a26167d96e00361fce22d3f45c43981bd731b2c
usmanrajaa <ansariusman858@gmail.com>
2025-07-28 18:29:13 +0530

    updated d3 and p3

--- chapter3/p3.sql
+++ chapter3/p3.sql
@@ -8,11 +8,13 @@ GROUP BY region;
 
 SELECT region,SUM(amount) AS total_amount
 FROM sales
-GROUP BY region;
+GROUP BY region
+HAVING SUM(amount)>20000;
 
 SELECT region,AVG(amount) AS total_amount
 FROM sales
-GROUP BY region;
+GROUP BY region
+HAVING AVG(amount)>10000;
 
 SELECT region,MAX(amount) AS total_amount
 FROM sales
@@ -21,3 +23,14 @@ GROUP BY region;
 SELECT region,MIN(amount) AS total_amount
 FROM sales
 GROUP BY region;
+
+SELECT
+CASE 
+WHEN amount<10000 THEN 'low'
+WHEN amount BETWEEN 10000 AND 20000 THEN 'medium'
+ELSE 'high'
+END AS salary_label,
+COUNT(*) AS COUNT
+FROM sales
+GROUP BY salary_label;
+
